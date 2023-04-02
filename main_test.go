95b55d6a2e395782a3243106fe9f28589541e27a
NViktorovich <nvmaslenko@gmail.com>
2023-04-02 17:49:39 +0500

    Текущая ветка: dev  Изменения, которые будут включены в коммит: 	изменено:      main.go 	изменено:      main_test.go
     Неотслеживаемые файлы:
    	.idea/

--- main_test.go
+++ main_test.go
@@ -3,9 +3,24 @@ package main
 import "testing"
 
 func TestGetSum(t *testing.T) {
-	got := GetSum(1)
-	want := 1
-	if got != want {
-		t.Errorf("got %d; want %d", got, want)
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{"zero args", args{[]int{}}, 0},
+		{"single arg", args{[]int{1}}, 1},
+		{"p and n args", args{[]int{-1, 1}}, 0},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := GetSum(tt.args.nums...); got != tt.want {
+				t.Errorf("GetSum() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
